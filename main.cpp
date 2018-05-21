c30ecbf3af269a8196a1062b0711fdff01df3237
NovokhatskyVA <37780632+NovokhatskyVA@users.noreply.github.com>
2018-05-21 20:13:28 +0300

    final

--- main.cpp
+++ main.cpp
@@ -1,58 +1,89 @@
 #include <iostream>
-#include <sstream>
-#include <cassert>
+#include <vector>
 using namespace std;
-char Scale[] = "CKF";
-struct Temperature{
-    double temp;
-    char scale;
-};
-istream & operator >> (istream & in,Temperature & Temp);
-istream & operator >> (istream & in,Temperature & Temp) {
-    in >> Temp.temp;
-    in >> Temp.scale;
-}
 
-void test_temperature_input()
-{
-    Temperature Temp;
-    string inp = "10C";
-    istringstream iss(inp);
-    iss >> Temp;
-    assert(Temp.temp==10);
-    assert(Temp.scale=='C');
-    string inp1 = "0K";
-    istringstream iss1(inp1);
-    iss1 >> Temp;
-    assert (Temp.temp == 0);
-    assert (Temp.scale =='K');
-    string inp2 = "-400F";
-    istringstream iss2 (inp2);
-    iss2 >> Temp;
-    assert (Temp.temp == -400);
-    assert (Temp.scale == 'F');
-}
-double convert (const Temperature & Temp,char scale_to)
-{
-    double Kelvins;
-    switch (Temperature.scale)
-    {
-    case 'K': {
-        Kelvins = ((9.0/5)(-273)+32);
-        break;
+#include "temperature.h"
+
+
+
+int
+main() {
+
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+
+    cerr << "Enter numbers: ";
+    vector<Temperature> numbers(number_count);
+    for (size_t i = 0; i < number_count; i++) {
+        cin >> numbers[i];
+        if (!cin) { cerr <<"Necorrectniyi vvod";
+        return 1; }
     }
-    case 'K1'
 
 
+    size_t column_count;
+    cerr << "Enter column count: ";
+    cin >> column_count;
 
-
-
-
+    Temperature min = numbers[0];
+    Temperature max = numbers[0];
+    for (Temperature number : numbers) {
+        if (number < min) {
+            min = number;
+        }
+        if (number > max) {
+            max = number;
+        }
     }
-}
-int main()
-{
-    test_temperature_input();
-}
 
+    max = convert(max, min.scale);
 
+    vector<size_t> counts(column_count);
+    for (Temperature number : numbers) {
+        number = convert(number, min.scale);
+        size_t column = (size_t)((number.value - min.value) / (max.value - min.value) *
+                column_count);
+        if (column == column_count) {
+            column--;
+        }
+        counts[column]++;
+    }
+
+    const size_t screen_width = 80;
+    const size_t axis_width = 4;
+    const size_t chart_width = screen_width - axis_width;
+
+    // Можно было бы считать в предыдущем цикле, но так более наглядно.
+    size_t max_count = 0;
+    for (size_t count : counts) {
+        if (count > max_count) {
+            max_count = count;
+        }
+    };
+    const bool scaling_needed = max_count > chart_width;
+
+    for (size_t count : counts) {
+        if (count < 100) {
+            cout << ' ';
+        }
+        if (count < 10) {
+            cout << ' ';
+        }
+        cout << count << "|";
+
+        size_t height = count;
+        if (scaling_needed) {
+            // Point: код должен быть в первую очередь понятным.
+            const double scaling_factor = (double)chart_width / max_count;
+            height = (size_t)(count * scaling_factor);
+        }
+
+        for (size_t i = 0; i < height; i++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
